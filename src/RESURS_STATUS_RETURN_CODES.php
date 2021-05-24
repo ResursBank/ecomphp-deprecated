2c154695cf0961b6cfdd826070685a395ff9f776
Tomas Tornevall <thorne@tornevall.net>
2021-05-24 10:54:15 +0200

    Ecom still use RESURS_STATUS_RETURN_CODES.

---
+++ src/RESURS_STATUS_RETURN_CODES.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Resursbank\RBEcomPHP;
+
+/**
+ * Class RESURS_STATUS_RETURN_CODES
+ * @package Resursbank\RBEcomPHP
+ * @deprecated Do not query this. Use Resursbank\Ecommerce\PaymentStatus or Resursbank\Ecommerce\OrderStatus.
+ */
+class RESURS_STATUS_RETURN_CODES extends RESURS_PAYMENT_STATUS_RETURNCODES
+{
+
+}
